5429ac598c255af00cabcfe93b1c421d67863f69
edwarsare <edwarsare@hotmail.com>
2022-07-27 03:02:46 -0500

    Lista mediante groups

--- ListViewDemo/Model/FriendRepository.cs
+++ ListViewDemo/Model/FriendRepository.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using ListViewDemo.Helpers;
 
 namespace ListViewDemo.Model
 {
@@ -62,11 +64,65 @@ namespace ListViewDemo.Model
 
         public List<Friend> GetFriendsByFirstLetter(string letter)
         {
+            
             var query = from q in Friends
                         where q.FirstName.StartsWith(letter)
                         select q;
 
             return query.ToList();
+        }
+
+        public ObservableCollection<Grouping<string,Friend>> GetAllGrouped()
+        {
+            Friends.Add(
+                new Friend()
+                {
+                    FirstName = "Juan",
+                    Phone = "5555000023"
+                }
+            );
+
+            Friends.Add(
+                new Friend()
+                {
+                    FirstName = "Maria",
+                    Phone = "5555000023"
+                }
+            );
+
+            Friends.Add(
+                new Friend()
+                {
+                    FirstName = "Pedro",
+                    Phone = "5555000023"
+                }
+            );
+
+            Friends.Add(
+                new Friend()
+                {
+                    FirstName = "Cesar",
+                    Phone = "5555000023"
+                }
+            );
+
+            Friends.Add(
+                new Friend()
+                {
+                    FirstName = "Julio",
+                    Phone = "5555000023"
+                }
+            );
+
+            var sorted = from f in Friends
+                         orderby f.FirstName
+                         group f by f.FirstName[0].ToString()
+                         into theGroup
+                         select new Grouping<string, Friend>(theGroup.Key, theGroup);
+
+            return new ObservableCollection<Grouping<string, Friend>>(sorted);
+
+            
         }
 
 
--- ListViewDemo/ViewModel/MainPageViewModel.cs
+++ ListViewDemo/ViewModel/MainPageViewModel.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using ListViewDemo.Helpers;
 using ListViewDemo.Model;
 
 namespace ListViewDemo.ViewModel
 {
     public class MainPageViewModel
     {
-        public IList<Friend> Friends { get; set; }
+        public ObservableCollection<Grouping<string, Friend>> Friends { get; set; }
 
         public MainPageViewModel()
         {
             FriendRepository repository = new FriendRepository();
-            Friends = repository.GetFriends().ToList();
+            Friends = repository.GetAllGrouped();
         }
 
 
